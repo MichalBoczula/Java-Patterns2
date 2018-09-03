9f27cc0d26829435c4a866b857880216d83559ee
MichalBoczula <michal.boczula@gmail.com>
2018-09-03 20:21:04 +0200

    mod 20 task 3 corrected

--- src/test/java/com/kodilla/patterns2/observer/homework/MentorTestSuite.java
+++ src/test/java/com/kodilla/patterns2/observer/homework/MentorTestSuite.java
@@ -11,15 +11,18 @@ public class MentorTestSuite {
         //Given
         final Course aprilCourse = new BootcampApril();
         final Course mayCourse = new BootcampMay();
-        final Mentor javaMentor = new Mentor("Java Mentor");
-        aprilCourse.registerObserver(javaMentor);
-        mayCourse.registerObserver(javaMentor);
+        final Mentor javaMentor1 = new Mentor("Java Mentor");
+        final Mentor javaMentor2 = new Mentor("Java Mentor");
+        aprilCourse.registerObserver(javaMentor1);
+        aprilCourse.registerObserver(javaMentor2);
+        mayCourse.registerObserver(javaMentor1);
         //When
         aprilCourse.addTask("mod 6 task 1");
         aprilCourse.addTask("mod 6 task 2");
         mayCourse.addTask("mod 1 task 2");
         mayCourse.addTask("mod 1 task 2");
         //Then
-        assertEquals(4, javaMentor.getUpdateCount());
+        assertEquals(4, javaMentor1.getUpdateCount());
+        assertEquals(2, javaMentor2.getUpdateCount());
     }
 }
